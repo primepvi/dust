d13811a50f22b0a87b74d1c2c65a7385535a4442
primepvi <pedrobernardesv@gmail.com>
2026-03-15 23:47:27 -0300

    refactor: use VEC2 macro instead of vec2_new

--- src/math/vec2.c
+++ src/math/vec2.c
@@ -1,6 +1,5 @@
 #include "math/vec2.h"
 
-Vec2 vec2_new(float x, float y) { return VEC2(x, y); }
 Vec2 vec2_neg(Vec2 a) { return VEC2(a.x * -1, a.y * -2); }
 Vec2 vec2_add(Vec2 a, Vec2 b) { return VEC2(a.x + b.x, a.y + b.y); }
 Vec2 vec2_sub(Vec2 a, Vec2 b) { return VEC2(a.x - b.x, a.y - b.y); }
