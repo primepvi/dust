527ceb07d2c07b5f611dbfdec6d971f26a5ccd42
primepvi <pedrobernardesv@gmail.com>
2026-03-19 14:04:03 -0300

    feat: implementing more functions on mat4.c

--- src/math/mat4.c
+++ src/math/mat4.c
@@ -1,56 +1,83 @@
 #include "math/mat4.h"
+#include <math.h>
+#include <stddef.h>
 
 // clang-format off
 Mat4 mat4_identity(void) {
   return (Mat4) {{
-      {1, 0, 0, 0},
-      {0, 1, 0, 0},
-      {0, 0, 1, 0},
-      {0, 0, 0, 1}
-    }};
+      1.0f, 0, 0, 0,
+      0, 1.0f, 0, 0,
+      0, 0, 1.0f, 0,
+      0, 0, 0, 1.0f
+     }};
 }
-
 
 Mat4 mat4_translate(Mat4 mat, Vec2 vec) {  
   Mat4 translation = {{
-      {1, 0, 0, 0},
-      {0, 1, 0, 0},
-      {0, 0, 1, 0 },
-      { vec.x, vec.y, 0, 1}
+      1.0f, 0, 0, 0,
+      0, 1.0f, 0, 0,
+      0, 0, 1.0f, 0 ,
+      vec.x, vec.y, 0, 1.0f
   }};
 
   return mat4_mul(mat, translation);
 }
 
-// clang-format on
+Mat4 mat4_scale(Mat4 mat, Vec2 scale) {
+  Mat4 s = (Mat4){{
+      scale.x, 0, 0, 0,
+      0, scale.y, 0, 0,
+      0, 0, 1.0f, 0,
+      0, 0, 0, 1.0f
+  }};
+
+  return mat4_mul(mat, s);
+}
+
+Mat4 mat4_rotate_z(Mat4 mat, float angle) {
+  Mat4 r = (Mat4){{
+      cosf(angle), sinf(angle), 0, 0,
+      -sinf(angle), cosf(angle), 0, 0,
+      0, 0, 1.0f, 0,
+      0, 0, 0, 1.0f
+  }};
+
+  return mat4_mul(mat, r);
+}
+
 Mat4 mat4_mul(Mat4 a, Mat4 b) {
   Mat4 c = {0};
 
-  for (unsigned int i = 0; i < 4; i++) {
-    c.elements[i][0] = (a.elements[i][0] * b.elements[0][0]) +
-                       (a.elements[i][1] * b.elements[1][0]) +
-                       (a.elements[i][2] * b.elements[2][0]) +
-                       (a.elements[i][3] * b.elements[3][0]);
-
-    c.elements[i][1] = (a.elements[i][0] * b.elements[0][1]) +
-                       (a.elements[i][1] * b.elements[1][1]) +
-                       (a.elements[i][2] * b.elements[2][1]) +
-                       (a.elements[i][3] * b.elements[3][1]);
-
-    c.elements[i][2] = (a.elements[i][0] * b.elements[0][2]) +
-                       (a.elements[i][1] * b.elements[1][2]) +
-                       (a.elements[i][2] * b.elements[2][2]) +
-                       (a.elements[i][3] * b.elements[3][2]);
-
-    c.elements[i][3] = (a.elements[i][0] * b.elements[0][3]) +
-                       (a.elements[i][1] * b.elements[1][3]) +
-                       (a.elements[i][2] * b.elements[2][3]) +
-                       (a.elements[i][3] * b.elements[3][3]);
+  for (size_t i = 0; i < 4; i++) {
+    for (size_t j = 0; j < 4; j++) {
+      for (size_t k = 0; k < 4; k++) {
+        mat4_index(c, i, j) += mat4_index(a, i, k) * mat4_index(b, k, j);
+      }
+    }
   }
 
   return c;
 }
 
-const float *mat4_ptr(const Mat4 *mat) {
-  return &mat->elements[0][0];
+Mat4 mat4_ortho(float l, float r, float b, float t) {
+  float n = -1.0f;
+  float f = 1.0f;
+  
+  return (Mat4){{
+      2/(r-l), 0, 0, 0,
+      0, 2/(t-b), 0, 0,
+      0, 0, (-2)/(f-n), 0,
+      -((r+l)/(r-l)), -((t+b)/(t-b)), -((f+n)/(f-n)), 1
+  }};
 }
+
+Mat4 mat4_view(Vec2 pos, float z) {
+  return (Mat4){{
+      z, 0, 0, 0,
+      0, z, 0, 0,
+      0, 0, 1, 0,
+      -pos.x * z, -pos.y * z, 0, 1
+  }};
+}
+
+const float *mat4_ptr(const Mat4 *mat) { return &mat->data[0]; }
